59d7b459d17dcd00c42b95395422029ae5e652ab
Guilherme Willian Castro <castro.ethng@gmail.com>
2024-05-05 20:50:38 -0300

    Update algoritmo_euclides 05-05.py

--- algoritmo_euclides 05-05.py
+++ algoritmo_euclides 05-05.py
@@ -1,5 +1,10 @@
+#algoritmo de euclides
+#Introdução à Teoria dos Números (2003, Santos, José Plínio Oliveira de)
 
 def alg_euclides(r0,r1):
-    print(f'{r0//r1}x{r1}+{r0-((r0//r1)*r1)}')
-    print("eof")
-alg_euclides(16,3)
+    if r1==0:
+        print("eof")
+    else:
+        print(f'{r0//r1}x{r1}+{r0-((r0//r1)*r1)}')
+        print("eof")
+alg_euclides(5,0)
