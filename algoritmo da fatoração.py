afc41141a92b5913eb9b6e6a520af817e67db5be
Guilherme Willian Castro <castro.ethng@gmail.com>
2024-09-01 11:56:39 -0300

    Update algoritmo da fatoração 09-01.py

--- algoritmo da fatoração.py
+++ algoritmo da fatoração.py
@@ -1,10 +1,21 @@
-#algoritmo de euclides
-#Introdução à Teoria dos Números (2003, Santos, José Plínio Oliveira de)
+## 
 
-def alg_euclides(r0,r1):
-    if r1==0:
-        print("eof")
-    else:
-        print(f'{r0//r1}x{r1}+{r0-((r0//r1)*r1)}')
-        print("eof")
-alg_euclides(5,2)
+n = 7
+f = 2
+
+try: val = int(n)
+except: print('insira um número');exit()
+N = int(n)
+if N < 2: print('insira um número maior do que 1')
+
+while True:
+    while True:
+        if n//f == n/f:print('f é fator trivial de n');break
+        else:
+            while True:
+                f+=1
+                if n//f == n/f:print('f é fator de n');break
+                if f**2>=n: print('n é primo');break
+            break
+    print(n,f)
+    break
