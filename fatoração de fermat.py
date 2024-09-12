088c771575ed755acb3addca58f52e349aa3f600
Guilherme Willian Castro <castro.ethng@gmail.com>
2024-09-11 21:28:23 -0300

    Update fatoração de fermat.py

--- fatoração de fermat.py
+++ fatoração de fermat.py
@@ -9,10 +9,6 @@ if x == n: print("n é composto, fim")
 
 else: x+=1; y = math.sqrt(x**2-n)
 while True:
-    #print(y)
-    #print(int(y))
-    #print(y-int(y))
     x+=1; y = math.sqrt(x**2-n)
-    #time.sleep(.5)
     if (x == ((n+1)/2)):print('n é primo');break 
     if ((y - int(y)) == 0):print('n é composto',x+int(y),',', x-int(y));break
